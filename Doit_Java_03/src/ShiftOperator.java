50a4eee920f5d225ac3d3c6ab1e0f399abbe35c9
kjh8673a <kjh8673a@naver.com>
2021-12-30 01:23:56 +0900

    03

---
+++ Doit_Java_03/src/ShiftOperator.java
@@ -0,0 +1,24 @@
+package src;
+
+public class ShiftOperator {
+    public static void main(String[] args) {
+        // 산술 시프트
+        // @ <<
+        System.out.println(3 << 1); // 6
+        System.out.println(-3 << 1); // -6
+        System.out.println(3 << 2); // 12
+        System.out.println(-3 << 2); // -12
+        System.out.println();
+
+        // @ >>
+        System.out.println(5 >> 1); // 2
+        System.out.println(-5 >> 1); // -3
+        System.out.println(5 >> 2); // 1
+        System.out.println(-5 >> 2); // -2
+        System.out.println();
+
+        // 논리 시프트 (>>>)
+        System.out.println(3 >>> 1); // 1
+        System.out.println(-3 >>> 31); // 1
+    }
+}
