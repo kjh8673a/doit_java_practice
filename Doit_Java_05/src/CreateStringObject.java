8e94adef13fc189424c8fb39dd8bb67ad693f255
kjh8673a <kjh8673a@naver.com>
2022-01-05 21:09:42 +0900

    05_2nd

---
+++ Doit_Java_05/src/CreateStringObject.java
@@ -0,0 +1,13 @@
+package src;
+
+public class CreateStringObject {
+    public static void main(String[] args) {
+        // String 객체 생성 1
+        String str1 = new String("안녕"); // new 키워드와 생성자를 이용
+        System.out.println(str1);
+
+        // String 객체 생성 2
+        String str2 = "안녕하세요"; // 문자열 리터럴을 바로 입력
+        System.out.println(str2);
+    }
+}
