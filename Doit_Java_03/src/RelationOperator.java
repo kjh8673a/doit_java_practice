a6ec7583f22621157569d9f66e4fc02c812e9a96
kjh8673a <kjh8673a@naver.com>
2022-01-10 14:04:19 +0900

    03

---
+++ Doit_Java_03/src/RelationOperator.java
@@ -0,0 +1,27 @@
+public class RelationOperator {
+    public static void main(String[] args) {
+        // 크기 비교
+        System.out.println(5 < 2); // false
+        System.out.println(5 > 2); // true
+        System.out.println(5 < 5); // false
+        System.out.println(5 <= 5); // true
+        System.out.println(5 >= 5); // true
+        System.out.println();
+
+        // 등가 비교
+        // @기본 자료형 등가 비교
+        int a = 5;
+        int b = 2;
+        int c = 5;
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        System.out.println(a == c); // true
+        System.out.println(a != c); // false
+        System.out.println();
+
+        // @참조 자료형 등가 비교
+        String str1 = new String("안녕");
+        String str2 = new String("안녕");
+        System.out.println(str1 == str2); // false
+    }
+}
