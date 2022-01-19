fb19d32704c0711f5ac23d0a92db10b52efd31ee
kjh8673a <kjh8673a@naver.com>
2022-01-20 01:06:48 +0900

    10

---
+++ Doit_Java_10/src/MethodOverriding_1/MthodOverriding_1.java
@@ -0,0 +1,30 @@
+package MethodOverriding_1;
+
+class A {
+    void print() {
+        System.out.println("A 클래스");
+    }
+}
+
+class B extends A {
+    @Override
+    void print() {
+        System.out.println("B 클래스");
+    }
+}
+
+public class MthodOverriding_1 {
+    public static void main(String[] args) {
+        // A 타입 / A 생성자
+        A aa = new A();
+        aa.print(); // A 클래스
+
+        // B타입 / B생성자
+        B bb = new B();
+        bb.print(); // B 클래스
+
+        // A 타입 / B 생성자
+        A ab = new B();
+        ab.print(); // B 클래스
+    }
+}
