60f568d0860e58dd70d4c1a59dc255fd4677e635
kjh8673a <kjh8673a@naver.com>
2022-02-18 18:26:02 +0900

    14

---
+++ Doit_Java_14/src/sec03_throwsexception/ex02_ThrowsException_2/ThrowsException_2.java
@@ -0,0 +1,14 @@
+package sec03_throwsexception.ex02_ThrowsException_2;
+
+public class ThrowsException_2 {
+    public static void main(String[] args) {
+        Class cls = Class.forName("java.lang.Object2");
+    }
+}
+
+/* 
+Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+        Unhandled exception type ClassNotFoundException
+
+        at sec03_throwsexception.ex02_ThrowsException_2.ThrowsException_2.main(ThrowsException_2.java:5)
+ */
