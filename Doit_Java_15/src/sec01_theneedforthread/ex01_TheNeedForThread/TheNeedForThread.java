419e719f4967cda28b5fd7c075ade94f568233ed
kjh8673a <kjh8673a@naver.com>
2022-02-19 20:47:57 +0900

    15

---
+++ Doit_Java_15/src/sec01_theneedforthread/ex01_TheNeedForThread/TheNeedForThread.java
@@ -0,0 +1,20 @@
+package sec01_theneedforthread.ex01_TheNeedForThread;
+
+public class TheNeedForThread {
+    public static void main(String[] args) {
+        // 비디오 프레임 1~5
+        int[] intArray = {1, 2, 3, 4, 5};
+        // 자막 번호 하나~다섯
+        String[] strArray = {"하나", "둘", "셋", "넷", "다섯"};
+        // 비디오 프레임 출력
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println("(비디오 프레임) " + intArray[i]);
+            try {Thread.sleep(200);} catch (InterruptedException e) {}
+        }
+        // 자막 번호 출력
+        for (int i = 0; i < strArray.length; i++) { // 비디오 프레임 번호가 모두 출력된 후 실행
+            System.out.println("(자막번호) " + strArray[i]);
+            try {Thread.sleep(200);} catch (InterruptedException e) {}
+        }
+    }
+}
